0103dcc7c91310a1a2cb5440240eb222c3bced93
Daniel Nissani <danieln2568@gmail.com>
2019-10-16 18:34:28 -0400

    Added some code to make it so we can search on a apkpure. Still need to debug.

--- web_scraper_1.py
+++ web_scraper_1.py
@@ -2,6 +2,24 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 import requests
+import urllib.request
+
+# List with google queries I want to make
+desired_google_queries = ['com.facebook.phone']
+
+for query in desired_google_queries:
+    # Constracting http query
+    url = 'https://apkpure.com/search?q=' + query
+    # For avoid 403-error using User-Agent
+    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+    response = urllib.request.urlopen( req )
+    html = response.read()
+    print(html)
+    # Parsing response
+    soup = BS(html, 'html.parser')
+    # Extracting number of results
+    resultStats = soup.find(id="resultStats").string
+    print(resultStats)
 
 url = 'https://apkpure.com/girlfriend-tracker-by-number/com.androidaplicativos.girlfriendtrackerpro'
 response = requests.get(url)
