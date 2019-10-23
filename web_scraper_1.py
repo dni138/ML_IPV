8842f88b2b5ddbc31616acf237d5ed8448ca35a4
Paula Barmaimon <paulabarmaimon@MacBook-Pro-de-Paula.local>
2019-10-22 22:43:49 -0400

    Scraped screenshots(links) for apkpure. OCR reads text from dowloaded images.

--- web_scraper_1.py
+++ web_scraper_1.py
@@ -14,8 +14,8 @@ for query in desired_google_queries:
     req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
     response = urllib.request.urlopen( req )
     html = response.read()
-    print(html)
     # Parsing response
+    print(html)
     soup = BS(html, 'html.parser')
     # Extracting number of results
     resultStats = soup.find(id="resultStats").string
@@ -26,7 +26,7 @@ response = requests.get(url)
 
 soup = BS(response.text, "lxml")
 
-#all_meta= soup.find_all('meta')
+#getting Meta Title, Meta Description, Keywords, App Description, og_image
 title = soup.title
 titleText = title.get_text()
 keywords = soup.find(attrs={"name":"keywords"}) 
@@ -34,11 +34,41 @@ description = soup.find(attrs={"name":"description"})
 description_body = soup.find("div", itemprop="description")
 og_image = soup.find(attrs={"property":"og:image"})
 
-print(titleText)
-print(keywords)
-print(description)
-print(description_body)
-print(og_image)
+#print(titleText)
+#print(keywords)
+#print(description)
+#print(description_body)
+#print(og_image)
+#print(imgs)
 
-#print(soup.prettify())
-#print(soup.body)
+#getting screenshots
+screenshots = []
+for link in soup.findAll('a', {'class': 'mpopup'}):
+    try:
+       screenshots.append(link['href'])
+    except KeyError:
+        pass
+print(screenshots)
+
+# Important comment: alt text is usually app Title + screenshot number. Give no additional info, so not included.
+# Same with Icon title and alt text
+
+# ------------------ OCR
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+#getting text from image
+def ocr_core(filename):
+  
+    text = pytesseract.image_to_string(Image.open(filename))
+    return text
+
+text_from_screenshots = ocr_core("screenhot_girlfriend.jpg")
+print(ocr_core(text_from_screenshots))
+
+#tesseract /Users/paulabarmaimon/Desktop/IPV/ML_IPV/screenhot_girlfriend.jpg out
+#im = Image.open("screenhot_girlfriend.jpg")
+#im.show()
