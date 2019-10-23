5a60d48d4eb9af95e864cda36ae67f34ba8b6dd5
Daniel Nissani <danieln2568@gmail.com>
2019-10-23 13:43:30 -0400

    Refactored code. We have a working scraper! Need to test more appids to build in correct error controls. We should also discuss how to integrate OCR.

--- web_scraper_1.py
+++ web_scraper_1.py
@@ -3,56 +3,63 @@ import pandas as pd
 from bs4 import BeautifulSoup as BS
 import requests
 import urllib.request
-
-# List with google queries I want to make
-desired_google_queries = ['com.facebook.phone']
-
-for query in desired_google_queries:
-    # Constracting http query
-    url = 'https://apkpure.com/search?q=' + query
-    # For avoid 403-error using User-Agent
-    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-    response = urllib.request.urlopen( req )
-    html = response.read()
-    # Parsing response
-    print(html)
-    soup = BS(html, 'html.parser')
-    # Extracting number of results
-    title = soup.title
-    titleText = title.get_text()
-    keywords = soup.find(attrs={"name":"keywords"}) 
-    description = soup.find(attrs={"name":"description"})
-    description_body = soup.find("div", itemprop="description")
-    og_image = soup.find(attrs={"property":"og:image"})
-    search_res = soup.find('div', id = 'search-res').find_all('a', href=True)
-
-    for href in search_res:
-        print(href['href'])
-    print(titleText)
-    #print(keywords)
-    print(description)
-    print(description_body)
-    print(og_image)
-    #print(search_res)
-
-#getting screenshots
-screenshots = []
-for link in soup.findAll('a', {'class': 'mpopup'}):
-    try:
-       screenshots.append(link['href'])
-    except KeyError:
-        pass
-print(screenshots)
-
-# Important comment: alt text is usually app Title + screenshot number. Give no additional info, so not included.
-# Same with Icon title and alt text
-
-# ------------------ OCR
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
+
+def parse_html_request(desired_queries, url):
+    '''
+    A fucntion that returns a list of html parsed BeautifulSoup objects.
+
+    Input: Desired list of queries (queries are strings represnting appids), suffix url
+
+    Output: List of soup objects from the desired queries
+    '''
+    soup = []
+    for query in desired_queries:
+        # Constracting http query
+        total_url = url + query
+        # For avoid 403-error using User-Agent
+        req = urllib.request.Request(total_url, headers={'User-Agent' : "Magic Browser"})
+        response = urllib.request.urlopen( req )
+        html = response.read()
+        # Parsing response
+        soup.append(BS(html, 'html.parser'))
+
+    return soup
+
+def grab_info(soup_objects, appids):
+    '''
+    Grab and format specific pieces of data from soup objects into json lines
+
+    Important comment: alt text is usually app Title + screenshot number. Give no additional info, so not included.
+    Same with Icon title and alt text
+
+    Input: list of soup objects, list of (strings) appids
+
+    Output: JSON lines containing appid, title, keywords, description, description_body, and og_image
+    '''
+    all_data = []
+    for soup_object, appid in zip(soup_objects, appids):
+        data = {}
+        data['title'] = soup_object.title.get_text()
+        data['keywords'] = soup_object.find(attrs={"name":"keywords"}) 
+        data['description'] = soup_object.find(attrs={"name":"description"})
+        data['description_body'] = soup_object.find("div", itemprop="description")
+        data['og_image'] = soup_object.find(attrs={"property":"og:image"})
+        screenshots = []
+        for link in soup_object.findAll('a', {'class': 'mpopup'}):
+            try:
+                screenshots.append(link['href'])
+            except KeyError:
+                pass
+        data['screenshots'] = screenshots
+        data['appid'] = appid
+        all_data.append(data)
+    
+    return all_data
 
 #getting text from image
 def ocr_core(filename):
@@ -60,9 +67,21 @@ def ocr_core(filename):
     text = pytesseract.image_to_string(Image.open(filename))
     return text
 
-text_from_screenshots = ocr_core("screenhot_girlfriend.jpg")
-print(ocr_core(text_from_screenshots))
+if __name__ == '__main__':
+    desired_queries = ['com.facebook.phone']
+    soup = parse_html_request(desired_queries, 'https://apkpure.com/search?q=')
+    new_queries = []
+    for html_query, query in zip(soup, desired_queries):
+        search_res = html_query.find('div', id = 'search-res').find_all('a', href=True)
+        for href in search_res:
+            if query in href['href']:
+                new_query = href['href']
+                break
+        new_queries.append(new_query)
 
-#tesseract /Users/paulabarmaimon/Desktop/IPV/ML_IPV/screenhot_girlfriend.jpg out
-#im = Image.open("screenhot_girlfriend.jpg")
-#im.show()
+    desired_soup = parse_html_request(new_queries, 'https://apkpure.com')
+
+    data = grab_info(desired_soup, desired_queries)
+
+    print(data[0]['screenshots'])
+            
